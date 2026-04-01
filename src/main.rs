58eb8674a0ad2d03ee122882a8ecda7429568e22
Coby Powers <mail@cobypowers.com>
2026-04-01 10:58:03 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,9 +10,11 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(mut stream) => loop {
+                let mut buf = String::new();
+                stream.read_to_string(&mut buf).unwrap();
                 stream.write_all("+PONG\r\n".as_bytes()).unwrap();
-            }
+            },
             Err(e) => {
                 println!("error: {}", e);
             }
